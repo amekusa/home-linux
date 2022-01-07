78e3d50cedd0474c2bd18c601e7cc39d17cb4bb8
amekusa <amekusa@users.noreply.github.com>
2022-01-07 17:10:43 +0900

    Move .update.sh to .sh/bin/update.sh

---
+++ .sh/bin/update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+REPOS=(
+  https://github.com/zsh-users/zsh-autosuggestions
+  https://github.com/zsh-users/zsh-syntax-highlighting
+  https://github.com/zsh-users/zsh-completions
+  https://github.com/agkozak/zsh-z
+  https://github.com/romkatv/powerlevel10k
+)
+
+BASE_DIR="$HOME/.sh"
+REPOS_DIR="$BASE_DIR/repos"
+
+[ -d "$REPOS_DIR" ] || mkdir -p "$REPOS_DIR"
+cd "$REPOS_DIR" || exit 1
+
+echo "Checking repositories..."
+
+for repo in "${REPOS[@]}"; do
+  echo
+  echo "=> $repo.git"
+  dir=$(basename "$repo")
+
+  if [ -d "$dir" ]; then
+    echo " -> Found at $PWD/$dir"
+    cd "$dir"
+    echo " -> Updating..."
+    git pull origin master
+    echo " -> Done."
+    cd ..
+
+  else
+    echo " -> Cloning into $PWD/$dir"...
+    git clone "$repo.git"
+    echo " -> Done."
+  fi
+
+done
+
+echo
+echo "All the repositories have been updated."
+echo
